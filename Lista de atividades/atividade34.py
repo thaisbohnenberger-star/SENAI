ac79bec894d07c543c193ef0c76c320eaf79bbc8
thaisbohnenberger-star <thais_bohnenberger@estudante.sesisenai.org.br>
2026-05-08 19:04:58 +0000

    update

--- Lista de atividades/atividade34.py
+++ Lista de atividades/atividade34.py
@@ -1,3 +1,6 @@
+tamanho_quadrado = float(input("Digite o lado do quadrado: "))
+area = tamanho_quadrado * tamanho_quadrado
 
+print("A área do seu quadrado é de:", area)
 
 #Faça um algoritmo que calcule e mostre a área de um quadrado. Sabe-se que: A = lado * lado;
--- Lista de atividades/atividade39.py
+++ Lista de atividades/atividade39.py
@@ -1,4 +1,12 @@
+salario = 1200
+c1 = 200
+c2 = 120
 
+total_contas = (c1 * 1.02) + (c2 * 1.02)
+restante = salario - total_contas
+
+print("Total das contas com multas: ", total_contas)
+print("Restará do salário do João: ", restante)
 
 #João recebeu seu salário de R$ 1200,00 e precisa pagar duas contas (C1=R$ 200,00 e C2=R$120,00) que 
 # estão atrasadas. Como as contas estão atrasadas, João terá de pagar multa de 2% sobre cada conta. 
